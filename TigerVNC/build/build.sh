2a9930784f4fa9663699c02f79eddead1ffef64a
CorcovadoMing <rf37535@gmail.com>
2016-04-12 23:09:20 +0000

    Add xorg-macro, xshmfence dependencies building script

--- TigerVNC/build/build.sh
+++ TigerVNC/build/build.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
+
+BUILD_XORG=0
+BUILD_X0VNC=1
+BUILD_XVNC=1
 
 HERE=`pwd`
 
+if [ $BUILD_X0VNC == 1 ]; then
 echo "=== Install nasm ==="
 tar xf nasm*.tar.* && cd nasm* && ./configure --prefix=${HERE} && make && make install && cd ..
 
@@ -18,10 +23,22 @@ tar xf fltk*.tar.* && cd fltk* && sed -i -e '/cat./d' documentation/Makefile && 
 
 echo "=== Install TigerVNC ==="
 tar xf tigervnc*.tar.* && cd tigervnc* && patch -Np1 -i ../tigervnc-1.6.0-xorg118-1.patch && patch -Np1 -i ../tigervnc-1.6.0-gethomedir-1.patch && mkdir -p build && cd build && cmake -G "Unix Makefiles" -DJPEG_INCLUDE_DIR=../../include -DJPEG_LIBRARY=../../lib/libjpeg.a -DCMAKE_INSTALL_PREFIX=${HERE} -DCMAKE_BUILD_TYPE=Release -Wno-dev .. && make && make install && cd ../../
+fi
 
+if [ $BUILD_XORG == 1 ]; then
+bash build-x-app.sh
+fi
+
+if [ $BUILD_XVNC == 1 ]; then
+echo "=== Install Xorg-macro ==="
+tar xf util-macro*.tar.* && cd util-macro* && ./configure $XORG_CONFIG --prefix=$HERE && make install && cd .. 
+
+echo "=== Install libxshmfence ==="
+tar xf libxshmfence-*.tar.* && cd libxshmfence* && ./configure $XORG_CONFIG --prefix=$HERE && make && make install && cd .. 
 
 echo "=== Install Xvnc ==="
-cd tigervnc* && cd build && cp -vR ../unix/xserver unix/ && tar -xf ../../xorg-server-1.17.2.tar.bz2 -C unix/xserver --strip-components=1 && cd unix/xserver && patch -Np1 -i ../../../unix/xserver117.patch && autoreconf -fi && ./configure $XORG_CONFIG \
+cd tigervnc* && cd build && cp -vR ../unix/xserver unix/ && tar -xf ../../xorg-server-1.17.2.tar.bz2 -C unix/xserver --strip-components=1 && cd unix/xserver && patch -Np1 -i ../../../unix/xserver117.patch && autoreconf -fi -I$HERE/share/aclocal \
+      && PKG_CONFIG_PATH=$HERE/lib/pkgconfig ./configure $XORG_CONFIG \
       --disable-xwayland    --disable-dri        --disable-dmx         \
       --disable-xorg        --disable-xnest      --disable-xvfb        \
       --disable-xwin        --disable-xephyr     --disable-kdrive      \
@@ -29,4 +46,6 @@ cd tigervnc* && cd build && cp -vR ../unix/xserver unix/ && tar -xf ../../xorg-s
       --disable-unit-tests  --disable-selective-werror                 \
       --disable-static      --enable-dri3                              \
       --without-dtrace      --enable-dri2        --enable-glx          \
-      --with-pic --prefix=${HERE} && make TIGERVNC_SRCDIR=`pwd`/../../../ && make install
+      --with-pic --prefix=${HERE} \
+      && make TIGERVNC_SRCDIR=`pwd`/../../../ && make install
+fi
--- TigerVNC/build/clean.sh
+++ TigerVNC/build/clean.sh
@@ -1,1 +1,1 @@
-rm -r bin fltk-1.3.3 include lib libjpeg-turbo-1.4.2 nasm-2.12.01 share tigervnc-1.6.0
+rm -r bin fltk-1.3.3 include lib libjpeg-turbo-1.4.2 nasm-2.12.01 share tigervnc-1.6.0 util-macros-1.19.0 libxshmfence-1.2
